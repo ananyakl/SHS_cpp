0c068e3be3f20520ebf1ab27af005963508b58d6
U-BSD\405581 <405581@SHS-DGCSCS2.beaverton.k12.or.us>
2022-10-12 12:23:40 -0700

    Got X (the first move) to work

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,35 +6,58 @@
 using namespace std;
 
 
-void game();
-void printBoard(bool testBool);
+void game(int (&board)[3][3], bool turn);
+void printBoard(bool testBool, char (&board)[3][3], bool turn);
 
 int main() {
   //int BLANK = 0;
   //int X_MOVE = 1;
   //int O_MOVE = 2;
-  int X_TURN = 0;
-  int O_TURN = 1;
-  int turn = X_TURN;
+  bool X_TURN = true;
+  //int O_MOVE = 1
+  bool turn = X_TURN;
   int X_SCORE = 0;
   int O_SCORE = 0;
+  char board[3][3];
+
+  for (int row = 0; row < 3; row++) {
+    for (int column = 0; column < 3; column++) {
+      board[row][column] = ' ';
+    }
+  }
 
   bool testBool = true;
 
-  printBoard(testBool);
+  printBoard(testBool, board, turn);
 
+  return 0;
 }
 
 
-void game(bool testBool) {
+void game(bool testBool, char (&board)[3][3], bool Xturn) {
   while (testBool == true) {
+    char BLANK = ' ';
+    char X_MOVE = 'X';
+    char O_MOVE = 'O';
+
     char input[2];;
     cout << "Enter the coordinate:  " << endl;
     cin >> input;
     cout << "You entered: " << input << endl;
+    int row = 0;
+    int column = 0;
     //Check if Coordinate is valid
-    if ( (((input[0] == 'a')||(input[0] == 'b') || (input[0] == 'c')) && ((input[1] == '1')||(input[1] == '2') || (input[1] == '3'))) && (strlen(input))) {
-      cout << "This works\n" << endl;
+    if ( ((((input[0] == 'a')||(input[0] == 'b') || (input[0] == 'c')) && ((input[1] == '1')||(input[1] == '2') || (input[1] == '3'))) && (strlen(input)))) {
+      if (input[0] == 'a') { row = 0;}
+      if (input[0] == 'b') { row = 1;}
+      if (input[0] == 'c') { row = 2;}
+      if (input[1] == '1') { column = 0;}
+      if (input[1] == '2') { column = 1;}
+      if (input[1] == '3') { column = 2;}
+
+      if (board[row][column] != ' ') { cout << "That is already filled. Try again" << endl; game(testBool, board, Xturn); }
+      else if (Xturn == true) { board[row][column] = X_MOVE; printBoard(testBool, board, false); }
+      else { board[row][column] = O_MOVE; printBoard(testBool, board, true); }
     }
     else {
       cout << "Enter a proper coordinate with a letter followed by a number\n" << endl;
@@ -45,12 +68,9 @@ void game(bool testBool) {
 
 
 
-void printBoard(bool testBool) {
-  int board[3][3];
-  int BLANK = 0;
-  int X_MOVE = 0;
-  int O_MOVE = 0;
+void printBoard(bool testBool, char (&board)[3][3], bool turn) {
 
+  
   
   char alpha[3] = {'a', 'b', 'c'};  //https://stackoverflow.com/questions/62006349/how-to-get-an-easier-way-of-generating-the-alphabet-in-c by user '463035818_is_not_a_number'
   cout << "  1 2 3" << endl;
@@ -58,9 +78,9 @@ void printBoard(bool testBool) {
   for (int row = 0; row < 3; row++) {
     cout << alpha[row] << " "; 
     for (int column = 0; column < 3; column++) {
-      cout << BLANK << " ";
+      cout << board[row][column] << " ";
     }
     cout << endl;
   }
-  game(testBool);
+  game(testBool, board, turn);
 }
