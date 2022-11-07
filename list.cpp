aff797b43e9317f1cde3841a862e806ec54a51fd
U-DESKTOP-RF0VQJ8\Me <lananyak@gmail.com>
2022-11-07 08:32:08 -0800

    final

--- list.cpp
+++ list.cpp
@@ -1,3 +1,7 @@
+//Student List
+//Add and remove students from list
+//Ananya lanka
+
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -5,7 +9,7 @@
 
 using namespace std;
 
-struct Student {
+struct Student { //creating a Student struct
   char firstName[10];
   char lastName[10];
   int studentId;
@@ -15,43 +19,46 @@ struct Student {
 void add(vector<Student*> &Vector);
 void del(vector<Student*> &Vector);
 void print(vector<Student*> &Vector);
-
+void askQuestion(vector<Student*> &Vector);
 
 int main() {
-  vector<Student*> Vector;
+  vector<Student*> Vector; //creating Student Vector
   bool playing = true;
-
-  while (playing == true) {
-    cout << "What do you want to do: ADD, DELETE, PRINT, or QUIT? " << endl;
-    char userInput[10];
-    cin.get(userInput, 10);
-
-    if (strcmp(userInput, "ADD")==0) {
-      cout << "adding" << endl;
-      add(Vector);
-    }
-    else if (strcmp(userInput, "DELETE")==0) {
-      cout << "deleting" << endl;
-      del(Vector);
-    }
-    else if (strcmp(userInput, "PRINT")==0) {
-      cout << "printing" << endl;
-      print(Vector);
-    }
-    else if (strcmp(userInput, "QUIT")==0) {
-      cout << "quiting" << endl;
-      playing = false;
-    }
-    else {
-      cout << "You can only ent+er the following commands: ADD, DELETE, PRINT, QUIT" << endl;
-    }
-  }
   
+  askQuestion(Vector);
+  cout << "Done.  " << endl;
+    
   return 0;
 }
 
-void add(vector<Student*> &Vector) {
-  Student* student = new Student;
+void askQuestion(vector<Student*> &Vector) { //asks user what they want to do 
+    cout << "What do you want to do: ADD, DELETE, PRINT, or QUIT? " << endl; 
+    char userInput[10];
+    cin >> userInput;
+
+    if (strcmp(userInput, "ADD")==0) { //if user inputs 'ADD'
+      cout << "adding" << endl;
+      add(Vector);
+    }
+    else if (strcmp(userInput, "DELETE")==0) { //if user inputs 'DELETE'
+      cout << "deleting" << endl;
+      del(Vector);
+    }
+    else if (strcmp(userInput, "PRINT")==0) { //if user inputs 'PRINT'
+      cout << "printing" << endl;
+      print(Vector);
+    }
+    else if (strcmp(userInput, "QUIT")==0) { //if user inputs 'QUIT'
+      cout << "quiting" << endl;
+    }
+    else {
+      cout << "You can only enter the following commands: ADD, DELETE, PRINT, QUIT" << endl;
+    }
+
+}
+
+void add(vector<Student*> &Vector) { //add student to vector
+  Student* student = new Student; //add new student object
   cout << "First Name: " << endl;
   cin >> student -> firstName;
   cin.ignore(10, '\n');
@@ -66,33 +73,34 @@ void add(vector<Student*> &Vector) {
   cin.ignore(4, '\n');
 
   Vector.push_back(student);  
+  askQuestion(Vector);
 }
 
-void print(vector<Student*> &Vector) {
-  for (vector<Student*>::iterator i = Vector.begin(); i != Vector.end(); i++) { //help recieved from Shreya Suresh
+void print(vector<Student*> &Vector) { //print all the students
+  for (vector<Student*>::iterator i = Vector.begin(); i != Vector.end(); i++) { // for length of vector, print student and information about studnet//code and help for all iterations rceived from Shreya Suresh
     cout << (*i) -> firstName << " ";
     cout << (*i) -> lastName << ",    ";
     cout << (*i) -> studentId << ",   ";
     //rounds gpa to 2 places
-    cout << fixed << setprecision(2) << (*i) -> gpa << endl;
-    
+    cout << fixed << setprecision(2) << (*i) -> gpa << endl;  
   }
+  askQuestion(Vector);
 }
 
-void del(vector<Student*> &Vector) {
+void del(vector<Student*> &Vector) { //find student number and delete, else not found
   cout << "Please enter student ID of the student you wish to remove." << endl;
   int num;
   cin >> num;
-  for (vector<Student*>::iterator i = Vector.begin(); i !=Vector.end(); i++) {
-    if ((*i) -> studentId == num) {
-      delete Vector[i-Vector.begin()];
+  for (vector<Student*>::iterator i = Vector.begin(); i !=Vector.end(); i++) { //for length of vector
+    if ((*i) -> studentId == num) { //if student ID is found
+      delete Vector[i-Vector.begin()]; //remove them
       Vector.erase(i);
       break;
     }
-    else {
+    else { //else student id is not found
       cout << "Student ID is not found. Please try again later. " << endl;
     }
 			       
   }
+  askQuestion(Vector);
 }
-
